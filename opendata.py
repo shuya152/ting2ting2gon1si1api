f9953ae4181c5e348287968ae09ccea89701f429
Chikorita152 <152shuya@gmail.com>
2024-12-08 21:41:35 +0800

    commit

--- opendata.py
+++ opendata.py
@@ -1,8 +1,8 @@
 import streamlit as st
+import pandas as pd
 import requests
 
 res = requests.get('https://ped.uspto.gov/api/queries/cms/public/13111111')
 data = res.json()
 
-st.dataframe(width='100%')
 st.table(data)
