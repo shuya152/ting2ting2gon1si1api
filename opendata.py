886532632708a4b2272908f5eacd41de024dabbc
Chikorita152 <152shuya@gmail.com>
2024-12-09 16:19:06 +0800

    commit

--- opendata.py
+++ opendata.py
@@ -1,8 +1,17 @@
 import pandas as pd
 import streamlit as st
 import requests
-res = requests.get('https://ped.uspto.gov/api/queries/cms/public/13111111')
-json = res.json()
-data:list = [json[0]]
+
+id:list = ['17021772','17205017','17475162']
+data:list = []
+
+for i in id:
+    res = requests.get(f'https://ped.uspto.gov/api/queries/cms/public/{i}')
+    if res.status_code == 200:
+        json = res.json()
+        data.append(json[0])
+    else:
+        print(f'回傳錯誤的id:{i}')
+
 df = pd.DataFrame(data)
 st.dataframe(df, use_container_width=True)
